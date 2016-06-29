4119f1129eef4848e72f605e7d84bd54f36ee306
Fuzzie Liu <loritally@126.com>
2016-06-29 13:03:26 +0800

    Initial Commit

---
+++ CircleLayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CircleLayer
+//
+//  Created by Fuzzie Liu on 16/6/29.
+//  Copyright © 2016年 Fuzzie Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
